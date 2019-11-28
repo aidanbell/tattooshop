980f929a8a0b8295f0cc1ee193bfe88b8abd0d45
aidanbell0 <aidanbell0182@gmail.com>
2019-11-28 11:28:13 -0500

    Added Aftercare Page

--- src/Pages/Aftercare/Aftercare.jsx
+++ src/Pages/Aftercare/Aftercare.jsx
@@ -1,8 +1,121 @@
 import React from 'react';
+import './Aftercare.css';
 
 const Aftercare = () => {
   return (
+    <div>
     <h1>AFTERCARE</h1>
+    <div class="aftercare">
+      <div class="healing">
+        <h3>For “Open Healing”:</h3>
+        <p>
+          Your tattoo artist will have applied an absorbent bandage to the tattooed
+          area after completion of the tattoo. You should keep this bandage on for
+          a minimum of 2 hours, maximum 5 hours. When you remove the bandage, wash
+          your tattoo throughly with warm water and an unscented antibacterial
+          liquid soap. Pat it dry with a clean paper towel, and wait dry 10-15
+          minutes until the skin feels tight. Apply a small amount of ointment or
+          tattoo cream to the area – just enough to thinly cover the tattoo. Use
+          just enough to keep the area moisturized – a small amount every 3-4 hours
+          should be sufficient. Repeat this process (wash, dry, and apply ointment)
+          2-3 times a day for the first 3 days after getting your tattoo. Starting
+          on day 4 you may reduce the washing to once a day/ as needed, but continue
+          to apply ointment every 3-4 hours.
+
+          Applying too much ointment can prevent the tattoo from healing properly.
+          Always wash your hands before applying ointment, and make sure the
+          tattooed area does not come in contact with potential contaminants like
+          clothing, pet hair, or unclean surfaces.
+        </p>
+        <hr/>
+          <h3>For “Recovery” (clear patch):</h3>
+          <p>
+            After completion of the tattoo, the artist will apply one recovery patch
+            entirely covering the tattooed area. For small tattoos
+            (less than 3 inches), this patch should be kept on the skin for 1-7 days,
+            according to your artist's instructions. Do not keep the covering on your
+            skin for more than 7 days. You can wear the patch in the shower and under
+            clothing, but do not scrub or submerge the area. Do not wear tight
+            clothing or shoes that will rub the tattooed area.
+
+            If the patch comes off, exposing any part of the tattooed area, remove
+            the patch. Wash the area thoroughly with an unscented liquid antibacterial
+            soap. Dry the area with a clean paper towel – pat dry, do not rub or wipe.
+            Wait 10-15 minutes until the skin is completely dry (it should feel
+            somewhat tight). If your artist has provided you with a second patch,
+            apply it to the clean and dry tattooed area. Remove the white backing
+            first, and then stick the patch to your tattoo, starting form the middle
+            and working out toward the edges, so as to avoid large air bubbles. There
+            should be a border of approximately 1/2 inch between the outer edge of
+            your tattoo and the edge of the patch.
+
+            If your artist has not provided you with a second patch, or for any reason
+            you choose not to apply a second patch, follow the instructions for “Open
+            Healing” after washing and drying your tattoo.
+
+            Allergic reactions to some tattoo inks, as well as the “Recovery” patch
+            are uncommon, but possible. If you experience excessive redness, pain, or
+            itching in the tattooed area, seek immediate medical attention.
+
+            If you have the “Recovery” patch on your skin and experience redness,
+            excessive pain, or itching in the area under the patch, remove the patch,
+            wash with an antibacterial liquid soap, dry thoroughly with a clean paper
+            towel, and continue to care for your tattoo using the “Open Healing”
+            method for at least 7-10 days from the day you received your tattoo. If
+            the condition persists for more than a day after removing the patch, you
+            should see a doctor.
+          </p>
+        </div>
+        <hr/>
+        <div class="warning">
+            It is possible for any tattoo to become infected if not cared for
+            properly. Infections are caused by bacteria, and can result from contact
+            with clothes, animal hair or saliva, dust, dirt, and any other
+            contaminated surfaces. If you think your tattoo has become infected, seek
+            immediate medical attention. In order to reduce the risk of infection,
+            wash your tattoo with an antibacterial liquid soap 1-3 times a day, and
+            pat dry with a clean paper towel. Always wash your hands before applying
+            ointment, and avoid contact with potential contaminants.
+        </div>
+        <hr/>
+        <div class="not-list">
+        <h3>What not to do:</h3>
+        <ul>
+          <li>
+            NO swimming, baths, or submerging in water for 7-10 days after getting
+            tattooed, or until the tattoo is fully healed (all scabs have fallen off
+            on their own)
+          </li>
+          <li>
+            NO sports or gyms for 7-10 days after getting tattooed, or until the
+            tattoo is healed (all scabs have fallen off on their own) Gyms are a
+            common source of bacteria – avoid them until your tattoo is completely
+            healed. Also avoid any strenuous activity, especially anything that will
+            cause you to sweat. There is bacteria in your sweat that can cause an
+            infection.
+          </li>
+          <li>
+            NO direct sunlight for 1-3 months after getting your tattoo. During this
+            time, your skin is still recovering, and is thinner than usual in the
+            tattooed area. Any prolonged sun exposure can cause excessive fading
+            during this time period. During the first 7-10 days after getting
+            tattooed, simply stay out of the sun or keep the area covered. After
+            the scabs have fallen off, you may use a 50+ spf sunscreen to protect
+            your tattoo.
+          </li>
+          <li>
+            DO NOT pick the scabs. If you remove the scabs from your tattoo, you will
+            pull out some of the ink as well, causing the tattoos to look faded or
+            patchy.
+          </li>
+          <li>
+            NO saunas, massages, scrubs or chemical peels for 10 days after getting
+            tattooed.
+          </li>
+      </ul>
+    </div>
+  </div>
+</div>
   )
 }
 
